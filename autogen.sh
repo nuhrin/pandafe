c065b70562561eea2df273eef5d0db99eb9e5a9e
nuhrin <nuhrin@oceanic.to>
2012-10-22 01:56:15 -0600

    workaround libgee submodule anomolies during autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,15 @@
 test -n "$srcdir" || srcdir=$(dirname "$0")
 test -n "$srcdir" || srcdir=.
 (
+  # initialize submodules and tweak libgee to allow autoreconf to complete
   test -d .git && git submodule update --init &&
-    mkdir submodules/libgee/m4 && touch submodules/libgee/ChangeLog
+    mkdir -p submodules/libgee/m4 && touch submodules/libgee/ChangeLog
+
+  # autoreconf
   cd "$srcdir" &&
   AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv -Wall
+
+  # discard change to libgee/INSTALL by autoreconf, to avoid git tracking module change
+  cd submodules/libgee && git checkout -- INSTALL
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
